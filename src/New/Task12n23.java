ff6f36280cabecc32564c2d5fefef61c5fa69ae4
ILONA <avvakumova_ilona@mail.ru>
2020-03-26 13:04:39 +0300

    решение

--- src/New/Task12n23.java
+++ src/New/Task12n23.java
@@ -2,6 +2,28 @@ package New;
 
 public class Task12n23 {
     public static void main(String[] args) {
+        int i;
+        int j;
+        int n;
+
+int array [] [] = new int[7][7];
+//for (i = 0; i < 7; i++){
+  //  for ( j =0; j < 7; j++){
+        for (i=0; i < array.length; i++){
+            for (j=0; j < array.length; j++){
+                if ((i+j)%6 == 0 )
+                    array [i][j] = 1;
+                 else if ( i==j )
+                    array [i][j] = 1;
+                else
+                array [i] [j] =0;
+
+      //  array [i] [j] = (i) + (j);
+        System.out.print(array [i] [j] + "\t");
+    }
+    System.out.println();
+}
+
 
     }
 }
