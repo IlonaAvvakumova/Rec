e402a10cdff01b908ddaa6a0d7e84f428e8590e0
ILONA <avvakumova_ilona@mail.ru>
2020-03-28 11:54:55 +0300

    решение1

--- src/New/Task12n25.java
+++ src/New/Task12n25.java
@@ -1,6 +1,16 @@
 package New;
+import java.util.*;
 
 public class Task12n25 {
+    public static void a (int  array [][], int n, int m) {
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int i;
         int j;
@@ -8,13 +18,18 @@ public class Task12n25 {
         int m = 12;
         int array[][] = new int[m][n];
         int k = 1;
+        System.out.println("Первый массив ");
         for (i = 0; i < m; i++) {
             for (j = 0; j < n; j++) {
                 array[i][j] = k++;
-
-                System.out.print(array[i][j] + "\t");
             }
-            System.out.println();
         }
+        a(array, n, m);
+
+
+        System.out.println("Второй массив ");
+
+
+
     }
 }
