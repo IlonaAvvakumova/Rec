fe9a726d6c7f53c98333ab5d062a9905e8598495
AmirovRenat <54325939+AmirovRenat@users.noreply.github.com>
2020-03-30 01:13:16 +0500

    Task12n25 updated. a) and b) done.

--- src/New/Task12n25.java
+++ src/New/Task12n25.java
@@ -2,53 +2,49 @@ package New;
 import java.util.*;
 
 public class Task12n25 {
-    private static int M = 12;
-    private static int N = 10;
+    private static int vDimension = 12;
+    private static int hDimension = 10;
 
     public static void main(String[] args) {
-        int [][] array = new int[M][N];
-        int k = 1;
+        createAndPrintArrayA();
+        createAndPrintArrayB();
+    }
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
+    private static void createAndPrintArrayA() {
+        int[][] array = new int[vDimension][hDimension];
+        int k = 1;
+        for (int i = 0; i < vDimension; i++) {
+            for (int j = 0; j < hDimension; j++) {
                 array[i][j] = k++;
             }
         }
-       print("Первый массив ",array);
 
-
-      int array2[][] = new int[M][M];
-        k = 1;
-        
-        for (int i = 0; i < M; i++) {
-
-            for (int j = 0; j < M; j++) {
-                array2[i][j] = k++;
-            }
-        }
-
-        for (int i = 0; i < M; i++) {
-
-            for (int j = i+1; j < M; j++) {
-                int temp = array2[i][j];
-                array2[i][j] = array2[j][i];
-                array2[j][i] = temp;
-            }
-        }
-        print ("Второй массив ", array2);
-
+        print("Первый массив", array);
     }
 
-    public static void  print (String comment, int  array [][]) {
+    private static void createAndPrintArrayB() {
+        int[][] array2 = new int[vDimension][hDimension];
+        int k = 1;
+
+        for (int i = 0; i < hDimension; i++) {
+            for (int j = 0; j < vDimension; j++) {
+                array2[j][i] = k++;
+            }
+        }
+
+        print("Второй массив", array2);
+    }
+
+    public static void print(String comment, int[][] array) {
         System.out.println(comment);
-        int m =array.length;
-        int n = array[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int vDim = array.length;
+        int hDim = array[0].length;
+
+        for (int i = 0; i < vDim; i++) {
+            for (int j = 0; j < hDim; j++) {
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
     }
-
 }
