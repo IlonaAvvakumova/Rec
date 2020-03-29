4d6bf40031222cfb60ffe20b603120cf402475ce
AmirovRenat <54325939+AmirovRenat@users.noreply.github.com>
2020-03-29 23:14:13 +0500

    Update Task12n25.java

--- src/New/Task12n25.java
+++ src/New/Task12n25.java
@@ -2,22 +2,10 @@ package New;
 import java.util.*;
 
 public class Task12n25 {
-    private static int N = 10;
     private static int M = 12;
+    private static int N = 10;
 
-    public static void  print (String comment, int  array [][]) {
-        System.out.println(comment);
-        int m =array.length;
-        int n = array[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(array[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
     public static void main(String[] args) {
-
         int [][] array = new int[M][N];
         int k = 1;
 
@@ -30,25 +18,37 @@ public class Task12n25 {
 
 
       int array2[][] = new int[M][M];
-         k = 1;
+        k = 1;
+        
         for (int i = 0; i < M; i++) {
 
             for (int j = 0; j < M; j++) {
                 array2[i][j] = k++;
-
             }
         }
+
         for (int i = 0; i < M; i++) {
 
             for (int j = i+1; j < M; j++) {
                 int temp = array2[i][j];
                 array2[i][j] = array2[j][i];
                 array2[j][i] = temp;
-
             }
-
         }
         print ("Второй массив ", array2);
 
     }
+
+    public static void  print (String comment, int  array [][]) {
+        System.out.println(comment);
+        int m =array.length;
+        int n = array[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
 }
